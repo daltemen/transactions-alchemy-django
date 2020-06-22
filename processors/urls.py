0113fe1904b5f2bfb437bcbcc09c98f4b3c36935
Daniel Mendoza <daltemen@gmail.com>
2020-06-22 01:38:28 -0500

    feat: processors lister service and pagination

--- processors/urls.py
+++ processors/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from processors.views import FileUploadView
+from processors.views import FileUploadView, TransactionsList
 
 urlpatterns = [
-    path('files', FileUploadView.as_view())
+    path("files", FileUploadView.as_view()),
+    path("transactions", TransactionsList.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
